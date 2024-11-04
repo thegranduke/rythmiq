4da2a5c53ac547233daa9ac178a1477e80bd05fd
Eino <einompi@gmail.com>
2024-11-04 14:09:43 +0200

    Create basic Video and Audio models

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Video(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    video_file = models.FileField(upload_to='videos/')
+    upload_date = models.DateTimeField(auto_now_add=True)
+
+class Audio(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    audio_file = models.FileField(upload_to='audios/')
+    upload_date = models.DateTimeField(auto_now_add=True)
